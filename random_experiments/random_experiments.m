4a4578fb306d8e5c204990423ac6ac5606fbec64
Daniel Arnström <daniel.arnstrom@liu.se>
2022-11-18 13:33:19 +0100

    Fix indentation.

--- random_experiments/random_experiments.m
+++ random_experiments/random_experiments.m
@@ -15,60 +15,54 @@ fvaldiff= zeros(length(dims),nruns,1);
 grb_params.Threads=1;
 grb_params.OutputFlag=0;
 for nrun = 1:nruns
-  i_dim = 0;
-  for dim = dims 
-	[nrun,dim]
-	i_dim = i_dim+1;
-	%generate problem
-	n = 5*dim;
-	m = 10*dim;
-	ms = dim;
-	%M = randn(n,n);
-	%H = M'*M;
-	H = full(sprandsym(n,1,1e-4,2));
-	f = 100*randn(n,1); 
-	f(1:ms) = -sign(f(1:ms)).*f(1:ms);
-	A = randn(m,n);
-	bupper = 20*rand(m,1);
-	blower = -20*rand(m,1);
-	bupper_tot = [ones(ms,1);bupper];
-	blower_tot = [zeros(ms,1);blower];
-	sense = int32(zeros(m+ms,1));
-	sense(1:ms) = sense(1:ms)+16;
-    d = daqp();
-    [~,setup_time] = d.setup(H,f,A,bupper_tot,blower_tot,sense);
-    d.settings('iter_limit',1e4);
-	[xstar,fval,exitflag,info] = d.solve();
-	%[xstar,fval,exitflag,info] = daqp.quadprog(H,f,A,bupper_tot,blower_tot,sense);
-	fval = 0.5*xstar'*H*xstar+f'*xstar;
-	times(i_dim,nrun,1)=setup_time+info.solve_time;
-	iters(i_dim,nrun,1)=info.iter;
-	nodes(i_dim,nrun,1)=info.nodes;
-	flags(i_dim,nrun,1)=exitflag;
+    i_dim = 0;
+    for dim = dims 
+        [nrun,dim]
+        i_dim = i_dim+1;
+        % generate problem
+        n = 5*dim;
+        m = 10*dim;
+        ms = dim;
 
-	% Solve with Gurobi
-	model.Q = 0.5*sparse(H);
-	model.A = sparse([A;-A]);
-	model.rhs = [bupper;-blower];
-	model.obj = f;
-	model.sense='<';
-	model.vtype=repelem('BC',[ms,n-ms]);
-	model.lb = -inf(n,1);
-	results = gurobi(model,grb_params);
-	times(i_dim,nrun,2)=results.runtime;
-	iters(i_dim,nrun,2)=results.itercount;
-	nodes(i_dim,nrun,2)=results.nodecount;
+        H = full(sprandsym(n,1,1e-4,2));
+        f = 100*randn(n,1); 
+        f(1:ms) = -sign(f(1:ms)).*f(1:ms);
+        A = randn(m,n);
+        bupper = 20*rand(m,1);
+        blower = -20*rand(m,1);
+        bupper_tot = [ones(ms,1);bupper];
+        blower_tot = [zeros(ms,1);blower];
+        sense = int32(zeros(m+ms,1));
+        sense(1:ms) = sense(1:ms)+16;
+        d = daqp();
+        [~,setup_time] = d.setup(H,f,A,bupper_tot,blower_tot,sense);
+        d.settings('iter_limit',1e4);
+        [xstar,fval,exitflag,info] = d.solve();
+        fval = 0.5*xstar'*H*xstar+f'*xstar;
+        times(i_dim,nrun,1)=setup_time+info.solve_time;
+        iters(i_dim,nrun,1)=info.iter;
+        nodes(i_dim,nrun,1)=info.nodes;
+        flags(i_dim,nrun,1)=exitflag;
 
-	xgrb = results.x;
-	fgrb = 0.5*xgrb'*H*xgrb+f'*xgrb;
+        % Solve with Gurobi
+        model.Q = 0.5*sparse(H);
+        model.A = sparse([A;-A]);
+        model.rhs = [bupper;-blower];
+        model.obj = f;
+        model.sense='<';
+        model.vtype=repelem('BC',[ms,n-ms]);
+        model.lb = -inf(n,1);
+        results = gurobi(model,grb_params);
+        times(i_dim,nrun,2)=results.runtime;
+        iters(i_dim,nrun,2)=results.itercount;
+        nodes(i_dim,nrun,2)=results.nodecount;
 
-	xdiff(i_dim,nrun,1) = norm(xstar-xgrb);
-	fvaldiff(i_dim,nrun,1) = fgrb-fval;  
+        xgrb = results.x;
+        fgrb = 0.5*xgrb'*H*xgrb+f'*xgrb;
 
-	% Solve with CPLEX
-	%[xcplex,~,~,output] = cplexmiqp(H,f,[A;-A],[bupper;-blower],[],[],[],[],[],[],[],repelem('BC',[ms,n-ms]));
-	%times(i_dim,nrun,3)=output.time;
-  end
+        xdiff(i_dim,nrun,1) = norm(xstar-xgrb);
+        fvaldiff(i_dim,nrun,1) = fgrb-fval;  
+    end
 end
 
 
